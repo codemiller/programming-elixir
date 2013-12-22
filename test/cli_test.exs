bb64245f4669511f41fc9009f275e2b7c2572ef8
Katie Miller <katie@codemiller.com>
2013-12-22 10:30:26 +1000

    Adding CLI tests

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -1,7 +1,8 @@
 defmodule CliTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
 
-  import Barcalc.CLI, only: [ parse_args: 1, parse_drinks: 2 ]
+  import Barcalc.CLI, only: [ run: 1, parse_args: 1, parse_drinks: 2 ]
 
   test ":help returned by --help switch" do
     assert parse_args(["--help", "foo"]) == :help
@@ -38,4 +39,18 @@ defmodule CliTest do
   test "Multiple drinks and quantities return result" do
     assert (parse_drinks(["wine", 1, "beer", 2], [])) == 4.580145  
   end 
+
+  test "Result is formatted to one decimal place" do
+    result = capture_io fn ->
+      run(["wine", 1])
+    end
+    assert result == "1.4 standard drinks\n"
+  end
+
+  test "Help includes drink list" do
+    result = capture_io fn ->
+      run(["foo"])
+    end
+    assert String.contains?(result, ["beer","margarita","wine"])
+  end
 end
