fc080485f2fd13a33d4b8ef8a98c92e9aa1ba293
Katie Miller <katie@codemiller.com>
2013-12-23 17:45:10 +1000

    Added macro example

---
+++ lib/barcalc/featuredemos/typo.ex
@@ -0,0 +1,19 @@
+defmodule Barcalc.Featuredemos.Typo do
+  @moduledoc """
+  Corrects if keyword typos.
+  """
+
+  defmacro fi(condition, clauses) do
+    quote do: if(unquote(condition), unquote(clauses))
+  end
+
+  defmacro iff(condition, clauses) do
+    quote do: if(unquote(condition), unquote(clauses))
+  end
+
+  defmacro if_inspect(condition, clauses) do
+    IO.inspect condition
+    IO.inspect clauses
+    quote do: if(unquote(condition), unquote(clauses))
+  end
+end
---
+++ lib/barcalc/featuredemos/typo_test.ex
@@ -0,0 +1,22 @@
+defmodule Barcalc.Featuredemos.TypoTest do
+  require Barcalc.Featuredemos.Typo
+  import Barcalc.Featuredemos.Typo
+
+  alcohol_grams = 10 
+   
+  fi 15 > alcohol_grams do 
+    IO.puts "15 grams is more than the alcohol in a standard drink."
+  end
+
+  iff (alcohol_grams < 5) do
+    IO.puts "A standard drink contains less than five grams of alcohol."
+  else
+    IO.puts "A standard drink contains at least five grams of alcohol."
+  end
+
+  if_inspect 10 == alcohol_grams do
+    IO.puts "A standard drink contains exactly 10 grams of alcohol."
+  else
+    IO.puts "A standard does not contain exactly 10 grams of alcohol."
+  end
+end
