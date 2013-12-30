7540cde2daf5bf4cac4772040fc6f3bf31d1f980
Katie Miller <katie@codemiller.com>
2013-12-30 18:02:15 +1000

    Adding records to immutability example

--- lib/barcalc/demos/immutability.ex
+++ lib/barcalc/demos/immutability.ex
@@ -1,3 +1,6 @@
+defrecord Liquid, name: :nil, alcohol_pc: 0
+defrecord Beverage, name: :nil, content: {Liquid.new, 0}
+
 sav_blanc = Liquid.new name: "Sauvignon Blanc", alcohol_pc: 11
 house_white = Drink.new name: "House White Wine", content: {sav_blanc, 150}
 special = house_white
