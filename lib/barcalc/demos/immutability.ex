54c301c6b49300ec3e115278d43aa2de370fdf14
Katie Miller <katie@codemiller.com>
2013-12-30 18:06:17 +1000

    More tweaks to immutability example

--- lib/barcalc/demos/immutability.ex
+++ lib/barcalc/demos/immutability.ex
@@ -1,11 +1,8 @@
-defrecord Liquid, name: :nil, alcohol_pc: 0
-defrecord Beverage, name: :nil, content: {Liquid.new, 0}
-
 sav_blanc = Liquid.new name: "Sauvignon Blanc", alcohol_pc: 11
 house_white = Drink.new name: "House White Wine", content: {sav_blanc, 150}
 special = house_white
 house_white.name "House White"
-house_white
+IO.inspect house_white
 house_white = house_white.name "House White"
-house_white
-special
+IO.inspect house_white
+IO.inspect special
