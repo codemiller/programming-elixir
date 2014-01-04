e61120a8dfdc9cc703b5405f21d87a91dfe72624
Katie Miller <katie@codemiller.com>
2014-01-05 09:51:46 +1000

    Record type fix

--- lib/barcalc/demos/immutability.ex
+++ lib/barcalc/demos/immutability.ex
@@ -1,5 +1,5 @@
 sav_blanc = Liquid.new name: "Sauvignon Blanc", alcohol_pc: 11
-house_white = Drink.new name: "House White Wine", content: {sav_blanc, 150}
+house_white = Beverage.new name: "House White Wine", content: {sav_blanc, 150}
 special = house_white
 house_white.name "House White"
 IO.inspect house_white
