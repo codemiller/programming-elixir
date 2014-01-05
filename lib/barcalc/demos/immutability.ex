4988d8792a82864b5e05b6b0580d5c3594772e4e
Katie Miller <katie@codemiller.com>
2014-01-05 11:16:01 +1000

    Editing for consistency

--- lib/barcalc/demos/immutability.ex
+++ lib/barcalc/demos/immutability.ex
@@ -1,4 +1,4 @@
-sav_blanc = Liquid.new name: "Sauvignon Blanc", alcohol_pc: 11
+sav_blanc = Liquid.new name: "Sauvignon Blanc", alcohol_pc: 11.5
 house_white = Beverage.new name: "House White Wine", content: {sav_blanc, 150}
 special = house_white
 house_white.name "House White"
